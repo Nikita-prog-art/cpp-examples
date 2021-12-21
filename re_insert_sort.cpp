be8b60a95cf43a101434f6d7857271f6305d12e5
Nikita Alabugin <65731906+Nikita-prog-art@users.noreply.github.com>
2021-12-21 14:10:58 +0500

    Create re_insert_sort.cpp

---
+++ re_insert_sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> mass(n);
+    for(int i = 0; i < n; i++)
+        cin >> mass[i];
+    int i = 1;
+    while(i < n) {
+        if(mass[i] > mass[i - 1]) {
+            int x = mass[i];
+            int j = i - 1;
+            while(j >= 0 && mass[j] < x) {
+                mass[j + 1] = mass[j];
+                j--;
+            }
+            mass[j + 1] = x;
+        }
+        i++;
+    }
+    for(auto x : mass)
+        cout << x << " ";
+    return 0;
+}
